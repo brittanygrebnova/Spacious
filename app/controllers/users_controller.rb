6d77ef2594750e71b9008f968075217fee924fb6
github username <github email address>
2019-05-06 11:24:19 -0400

    adds new create show and getcurrentuser methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,29 @@
 class UsersController < ApplicationController
 
-  def index
-    users = User.all
-    render json: users
+  def new
+    user = User.new
   end
 
   def create
+    user = User.create(user_params)
+    if user
+      session[:user_id] = user.id
+    end
   end
+
+  def show
+    render json: get_current_user
+  end
+
 
   private
 
   def user_params
     params.require(:user).permit(:firstname, :lastname, :username)
+  end
+
+  def get_current_user
+    User.find(session[:user_id])
   end
 
 
