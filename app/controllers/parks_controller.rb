ec839e3099eb6c271fd3f0a8b00302c57b72534b
github username <github email address>
2019-05-06 11:35:50 -0400

    adds favorite method to users controller

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -5,7 +5,26 @@ class ParksController < ApplicationController
     render json: parks
   end
 
+  def show
+    park = Park.find(params[:park][:park_code])
+    render json: park
+  end
+
   def favorite
+    type = params[:type]
+    park = params[:park]
+    if type == "favorite"
+      current_user.favorites << park
+      render json: current_user.favorites
+
+    elsif type == "unfavorite"
+      current_user.favorites.delete(park)
+      render json: current_user.favorites
+
+    else
+      # Type missing, nothing happens
+      redirect_to :index
+    end
   end
 
 end
