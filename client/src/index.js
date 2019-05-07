ff0bb5dd847a46dcff543d527b4fc9030c70e088
github username <github email address>
2019-05-06 23:44:08 -0400

    adds login container component

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-export default App;
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -7,16 +6,16 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
-// import rootReducer from './reducers'
+import rootReducer from './reducers'
 
-// const store = createStore(rootReducer, applyMiddleware(thunk))
-//
-// ReactDOM.render(
-//   <Provider>
-//     <App/>
-//   </Provider>,
-//   document.getElementById('root')
-// )
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+  <Provider>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
