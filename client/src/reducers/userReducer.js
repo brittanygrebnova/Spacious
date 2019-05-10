1db17caf2ae0ade56e6a3f75159af0199e84126d
github username <github email address>
2019-05-10 10:20:49 -0400

    updated userReducer variable names

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,16 +1,16 @@
 const initialState = {
-  current: {},
-  favorites: []
+  currentUser: {},
+  currentUserFavorites: []
 }
 
 export default function userReducer(state = initialState, action) {
   switch(action.type) {
     case 'SET_USER':
-      return { ...state, current: action.payload }
+      return { ...state, currentUser: action.payload }
     case 'ADD_TO_FAVORITES':
-      return { ...state, favorites: [...state.favorites, action.payload] }
+      return { ...state, currentUserfavorites: [...state.currentUserfavorites, action.payload] }
     case 'REMOVE_FROM_FAVORITES':
-      return { ...state, favorites: state.favorites.filter(park => park.id !== action.payload.id) }
+      return { ...state, currentUserfavorites: state.currentUserfavorites.filter(park => park.id !== action.payload.id) }
     default: return state
   }
 }
