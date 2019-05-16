eb5f21d598e80207f8c29c9d5516498be10889ba
github username <github email address>
2019-05-15 21:54:37 -0400

    adds favorites header and adds it to favorite parks

--- client/src/components/FavoriteParks.js
+++ client/src/components/FavoriteParks.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ParkDetail from '../components/ParkDetail'
+import FavoritesHeader from '../components/FavoritesHeader'
 import {connect} from 'react-redux'
 
 class FavoriteParks extends Component {
@@ -9,30 +10,33 @@ class FavoriteParks extends Component {
     console.log(this.props.currentUser)
       if (this.props.currentUser) {
         return (
-          <div className="ui cards">
-            {this.props.currentUser[0].favorites.map((park, index) =>
-                <div className="card">
-                  <div className="content">
-                    <div className="header">{park.name}</div>
-                    <div className="description">
-                      <strong>Description: </strong>{park.description}
+          <div>
+            <div><FavoritesHeader/></div>
+            <div className="ui cards">
+              {this.props.currentUser[0].favorites.map((park, index) =>
+                  <div className="card">
+                    <div className="content">
+                      <div className="header">{park.name}</div>
+                      <div className="description">
+                        <strong>Description: </strong>{park.description}
+                      </div>
+                      <div className="description">
+                        <strong>States: </strong>{park.states}
+                      </div>
+                      <div className="description">
+                        <strong>Directions: </strong><a href={park.directionsUrl} target="_blank">{park.directionsUrl}</a>
+                      </div>
+                      <div className="description">
+                        <strong>Weather: </strong>{park.weatherInfo}
+                      </div>
                     </div>
-                    <div className="description">
-                      <strong>States: </strong>{park.states}
-                    </div>
-                    <div className="description">
-                      <strong>Directions: </strong><a href={park.directionsUrl} target="_blank">{park.directionsUrl}</a>
-                    </div>
-                    <div className="description">
-                      <strong>Weather: </strong>{park.weatherInfo}
+                    <div className="ui bottom attached button">
+                      <i className="add icon"></i>
+                      Add A Review
                     </div>
                   </div>
-                  <div className="ui bottom attached button">
-                    <i className="add icon"></i>
-                    Add A Review
-                  </div>
-                </div>
-              )}
+                )}
+              </div>
             </div>
           )
         }
