38a9a7f993e85822f9814902ac115698cbc6a5eb
github username <github email address>
2019-06-20 20:26:47 -0400

    updates more capitalization

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,69 +1,79 @@
-import React, {Component} from "react";
-import '../Login.css';
-import { fetchUsers, setUser } from '../actions/UserActions'
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux'
+import React, { Component } from "react";
+import "../Login.css";
+import { fetchUsers, setUser } from "../actions/userActions";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 class Login extends Component {
-
   componentDidMount() {
-    this.props.fetchUsers()
+    this.props.fetchUsers();
   }
 
   handleChange(event) {
-    console.log(event.target.value)
-    this.props.setUser(event.target.value)
+    console.log(event.target.value);
+    this.props.setUser(event.target.value);
   }
 
   render() {
-
-    console.log(this.props.allUsers)
+    console.log(this.props.allUsers);
 
     const mapUsersForDropdown = this.props.allUsers.map((user, index) => {
       return (
         <option value={user.id} key={user.id} id={user.id}>
           {user.firstname} {user.lastname}
         </option>
-      )
-    })
+      );
+    });
 
     const sayHiToCurrentUser = () => {
-      console.log(this.props.currentUser)
+      console.log(this.props.currentUser);
       if (this.props.currentUser) {
-        const username = this.props.currentUser.firstname
+        const username = this.props.currentUser.firstname;
         return (
           <div className="ui blue message">
-            <div className="header">
-              Welcome back, {username}!
-            </div>
-            <p>You can navigate to All Parks to find new parks or go to Favorites to see your list of saved parks</p>
+            <div className="header">Welcome back, {username}!</div>
+            <p>
+              You can navigate to All Parks to find new parks or go to Favorites
+              to see your list of saved parks
+            </p>
           </div>
-        )
+        );
       }
-    }
+    };
 
     return (
       <div>
-        <select className="ui search dropdown" onChange={(e) => this.handleChange(e)}>
+        <select
+          className="ui search dropdown"
+          onChange={e => this.handleChange(e)}
+        >
           <option value="">Find Your Name</option>
           {mapUsersForDropdown}
-        </select><br/>
+        </select>
+        <br />
         <div>{sayHiToCurrentUser()}</div>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     allUsers: state.user.allUsers,
     currentUser: state.user.currentUser
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => bindActionCreators({
-  fetchUsers,
-  setUser
-}, dispatch)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      fetchUsers,
+      setUser
+    },
+    dispatch
+  );
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
--- client/src/components/ParkDetail.js
+++ client/src/components/ParkDetail.js
@@ -1,65 +1,91 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { addParkToUserFavorites } from '../actions/UserActions'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { addParkToUserFavorites } from "../actions/userActions";
 
 class ParkDetail extends Component {
-
   render() {
-
     const renderParkDetail = () => {
-
-        if (this.props.selectedPark && this.props.currentUser.favorites) {
-          const checkFavorites = this.props.currentUser.favorites.filter(park => park.name === this.props.selectedPark.name)
-          console.log(checkFavorites)
-          return (
-            <div className="ui cards">
-              <div className="card">
-                <div className="content">
-                  <div className="header">{this.props.selectedPark.name}</div>
-                  <div className="description">
-                    <strong>Description: </strong>{this.props.selectedPark.description}
-                  </div>
-                  <div className="description">
-                    <strong>States: </strong>{this.props.selectedPark.states}
-                  </div>
-                  <div className="description">
-                    <strong>Directions: </strong><a href={this.props.selectedPark.directionsUrl} target="_blank">{this.props.selectedPark.directionsUrl}</a>
-                  </div>
-                  <div className="description">
-                    <strong>Weather: </strong>{this.props.selectedPark.weatherInfo}
-                  </div>
+      if (this.props.selectedPark && this.props.currentUser.favorites) {
+        const checkFavorites = this.props.currentUser.favorites.filter(
+          park => park.name === this.props.selectedPark.name
+        );
+        console.log(checkFavorites);
+        return (
+          <div className="ui cards">
+            <div className="card">
+              <div className="content">
+                <div className="header">{this.props.selectedPark.name}</div>
+                <div className="description">
+                  <strong>Description: </strong>
+                  {this.props.selectedPark.description}
                 </div>
-              <div>{checkFavorites.length > 0 ? <div className="ui bottom attached button"><i className="heart icon"></i>One Of Your Favorites</div> : <div className="ui bottom attached button" onClick={() => {
-                console.log('A'); this.props.addParkToUserFavorites(this.props.selectedPark, this.props.currentUser); console.log('B');}}>
-                <i className="add icon"></i>
-                Add To Favorites</div> }
+                <div className="description">
+                  <strong>States: </strong>
+                  {this.props.selectedPark.states}
+                </div>
+                <div className="description">
+                  <strong>Directions: </strong>
+                  <a
+                    href={this.props.selectedPark.directionsUrl}
+                    target="_blank"
+                  >
+                    {this.props.selectedPark.directionsUrl}
+                  </a>
+                </div>
+                <div className="description">
+                  <strong>Weather: </strong>
+                  {this.props.selectedPark.weatherInfo}
+                </div>
+              </div>
+              <div>
+                {checkFavorites.length > 0 ? (
+                  <div className="ui bottom attached button">
+                    <i className="heart icon" />One Of Your Favorites
+                  </div>
+                ) : (
+                  <div
+                    className="ui bottom attached button"
+                    onClick={() => {
+                      console.log("A");
+                      this.props.addParkToUserFavorites(
+                        this.props.selectedPark,
+                        this.props.currentUser
+                      );
+                      console.log("B");
+                    }}
+                  >
+                    <i className="add icon" />
+                    Add To Favorites
+                  </div>
+                )}
               </div>
             </div>
           </div>
-        )
+        );
       }
-    }
+    };
 
-    return (
-      <div>
-        {renderParkDetail()}
-      </div>
-    )
+    return <div>{renderParkDetail()}</div>;
   }
-
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     selectedPark: state.parks.selectedPark,
     currentUser: state.user.currentUser
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => bindActionCreators({
-  addParkToUserFavorites
-}, dispatch)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      addParkToUserFavorites
+    },
+    dispatch
+  );
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(ParkDetail)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ParkDetail);
